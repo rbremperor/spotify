2c476086d4f7e4af5ebaf066fc6542ba94155d15
Raxmatillo <rakhmatilloburkhonov@gmail.com>
2025-03-03 11:35:21 +0500

    added more tests, added openAPI documentation, change of requirements

--- spotify/settings.py
+++ spotify/settings.py
@@ -22,13 +22,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party
     'rest_framework',
+    'drf_yasg',
+
+    # local
     'music',
 ]
 
--- spotify/urls.py
+++ spotify/urls.py
@@ -1,9 +1,27 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Spotify-like API",
+        default_version='v1',
+        description="Swagger docs for REST API",
+        contact=openapi.Contact(email="rahmatilloburhonov3@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public = True,
+    permission_classes = [permissions.AllowAny,],
+)
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('music.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-swagger'),
 ]
